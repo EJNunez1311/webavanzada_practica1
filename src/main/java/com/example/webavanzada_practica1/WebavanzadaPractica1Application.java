fe5042903753a4a75c694cab6982e01824a3dcff
Edgar Núñez <EJNunez1311@users.noreply.github.com>
2019-10-09 23:01:34 -0400

    Initial commit

---
+++ src/main/java/com/example/webavanzada_practica1/WebavanzadaPractica1Application.java
@@ -0,0 +1,13 @@
+package com.example.webavanzada_practica1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebavanzadaPractica1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebavanzadaPractica1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webavanzada_practica1/WebavanzadaPractica1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.webavanzada_practica1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebavanzadaPractica1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
