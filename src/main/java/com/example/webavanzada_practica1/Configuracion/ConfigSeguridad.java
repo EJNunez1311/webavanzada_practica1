0956387b8bc1503a8facb18e6f6d3ce68cd6b879
Edgar Núñez <EJNunez1311@users.noreply.github.com>
2019-10-10 12:08:16 -0400

    Create ConfigSeguridad.java
    Creando ConfigSeguridad!

---
+++ src/main/java/com/example/webavanzada_practica1/Configuracion/ConfigSeguridad.java
@@ -0,0 +1,19 @@
+package com.example.webavanzada_practica1.Configuracion;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configurable
+@EnableGlobalMethodSecurity (securedEnabled = true) //Implementando SpringSecurity
+public class ConfigSeguridad extends WebSecurityConfigurerAdapter {
+
+    //Configuracion JPA para implementar Servicio Usuario
+    @Autowired
+}
